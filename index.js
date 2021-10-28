4bfd1a15927863a3c0fe4435ee67046dc2fe3b2c
Wkb2317 <2576267399@qq.com>
2021-10-28 20:43:30 +0800

    test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import neatCsv from 'neat-csv';
+import {Parser}  from "json2csv"
 import fs from 'fs';
 
 // 取文件夹下的文件
@@ -37,8 +38,8 @@ function replace(prevData,nextData){
   prevData.forEach((file,fileIndex) => {
     if(file.data.length != 0){
       file.data.forEach((item,dataIndex)=>{
-        item.lon_bd = nextData[fileIndex]["data"][dataIndex]["经度"];
-        item.lat_bd = nextData[fileIndex]["data"][dataIndex]["纬度"];
+        item.lon_bd = nextData[fileIndex]["data"][dataIndex]["����"];
+        item.lat_bd = nextData[fileIndex]["data"][dataIndex]["γ��"];
       })
     }
   });
@@ -49,20 +50,20 @@ let originalData = []; // 原文件 数据
 let data_84 = []; //  84坐标数据
 
 // 拿到data目录下的所有文件名
-let originalFilesName = getFilesName('data');
+let originalFilesName = getFilesName('input');
 let files84Name = getFilesName('84point');
 
 // 调用取数据函数  
-await getFilesData(originalFilesName, originalData, 'data'); // 取出原始数据（data文件夹下）
+await getFilesData(originalFilesName, originalData, 'input'); // 取出原始数据（input文件夹下）
 await getFilesData(files84Name, data_84, '84point'); //  取出的84坐标数据（84point文件夹下）
-// console.log(originalData, data_84);
 
 // 调用替换函数
 replace(originalData,data_84);
 
-data_84.forEach((item,index)=>{
-  // console.log(item["data"][index]["lon_bd"]);
-  console.log(item);
+// 输出
+const json2csvParser = new Parser();
+originalData.forEach((item)=>{
+	let csv = json2csvParser.parse(item.data);
+	fs.writeFileSync(`./output/${item.name}.csv`,csv);
+	console.log(item.name + "  文件处理成功  ");
 })
-
-
